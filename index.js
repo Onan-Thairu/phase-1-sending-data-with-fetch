c63317e8db7596ea0d9b9679aa09570dc1e7fa5c
OnanThairu <thairuonan@gmail.com>
2022-06-17 14:46:40 +0300

    sending data with fetch()

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(username, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('body').innerHTML = `<p>${data.id}</p>`
+    })
+    .catch(error => {
+        document.querySelector('body').innerHTML = `<p>${error.message}</p>`
+    })
+}
